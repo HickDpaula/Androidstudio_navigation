15500d53555d6d4b7475b2a7a5d73c17faa832dc
Hickdpaula <hickdpaula@gmail.com>
2022-09-29 17:51:44 -0300

    App TODO

--- app/src/main/java/com/example/todoandroid/ListFragment.kt
+++ app/src/main/java/com/example/todoandroid/ListFragment.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.findFragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todoandroid.adapter.TarefaAdapter
+import com.example.todoandroid.databinding.FragmentListBinding
+import com.example.todoandroid.model.Tarefa
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class ListFragment : Fragment() {
 
+    private lateinit var binding: FragmentListBinding
 
 
 
@@ -20,17 +24,28 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        binding = FragmentListBinding.inflate(layoutInflater, container, false )
 
-        val view = inflater.inflate(R.layout.fragment_list, container, false)
 
-        val floatingAdd = view.findViewById<FloatingActionButton>(R.id.floatingAdd)
 
-        floatingAdd.setOnClickListener {
+
+          //configuração do RecycleView
+
+        val adapter = TarefaAdapter()
+        binding.recyclerTarefa.layoutManager = LinearLayoutManager(context)
+        binding.recyclerTarefa.adapter = adapter
+        binding.recyclerTarefa.setHasFixedSize(true)
+
+
+
+        binding.floatingAdd.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_formFragment)
         }
 
 
-        return view
+
+
+        return binding.root
     }
 
 
